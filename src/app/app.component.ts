237223b51cf12fdc5c1211a1f84f3d0da17e0262
tatas-creativity <tatas.creativity@gmail.com>
2022-03-20 18:13:13 +0400

    second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -59,10 +59,18 @@ export class AppComponent {
     this.cards[0].toDo.splice(i, 1);
 
     this.cards[1].active = true;
+
+    if (this.cards[0].toDo?.length == 0) {
+      this.cards[0].active = false;
+    }
   }
 
   delete(i) {
     this.cards[0].toDo.splice(i, 1);
+
+    if (this.cards[0].toDo?.length == 0) {
+      this.cards[0].active = false;
+    }
   }
 
   toDone(i) {
@@ -71,17 +79,35 @@ export class AppComponent {
     this.cards[1].inProgress.splice(i, 1);
 
     this.cards[2].active = true;
+
+    if (this.cards[1].inProgress?.length == 0) {
+      this.cards[1].active = false;
+    }
   }
 
   toToDo(i) {
     let pushItem = this.cards[1].inProgress[i];
     this.cards[0].toDo?.push(pushItem);
     this.cards[1].inProgress.splice(i, 1);
+
+    if (this.cards[1].inProgress?.length == 0) {
+      this.cards[1].active = false;
+    }
+    if (this.cards[0].toDo?.length != 0) {
+      this.cards[0].active = true;
+    }
   }
 
   toBackInProgress(i) {
     let pushItem = this.cards[2].done[i];
     this.cards[1].inProgress?.push(pushItem);
     this.cards[2].done.splice(i, 1);
+
+    if (this.cards[2].done?.length == 0) {
+      this.cards[2].active = false;
+    }
+    if (this.cards[1].inProgress?.length != 0) {
+      this.cards[1].active = true;
+    }
   }
 }
